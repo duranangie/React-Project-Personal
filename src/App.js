18a726eaf0686bc9e95ed2e04b36639e02ad2dd2
duranangie <117625258+duranangie@users.noreply.github.com>
2024-05-15 17:03:39 -0500

    feat: :sparkles: add banner
    add banner and style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './Componats/NavBar';
+import {Banner} from './Componats/Banner'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar /> 
+      <Banner/>
     </div>
   );
 }
