2c7a29d932ddb10b2d902b4bb6d780cc299a0ad4
duranangie <duranangie>
2024-06-10 02:32:24 -0500

    feat: :sparkles: modificar url de project

--- src/Componats/ProjectsCard.js
+++ src/Componats/ProjectsCard.js
@@ -1,13 +1,17 @@
 import { Col} from "react-bootstrap"
 
-export const ProjectsCard = ({title,description, imgUrl}) =>{
+export const ProjectsCard = ({title,description, imgUrl, url}) =>{
     return(
         <Col sm={6} md={4} >
             <div className="proj-imgbx">
                 <img src={imgUrl}/>
                 <div className="proj-txtx">
-                    <h4>(title)</h4>
-                    <span>(description)</span>
+                    <h4>{title}</h4>
+                    <span>{description}</span>
+                    <br></br>
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+            Ir a {title}
+          </a>
                 </div>
             </div>
         </Col>
